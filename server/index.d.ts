bffd80e7eeea857d79ccbe9ee2e17d2976a5aeea
eduzayn <ti@eduzayn.com.br>
2025-05-04 16:20:03 -0300

    Delete server directory

--- server/index.d.ts
+++
@@ -1,7 +0,0 @@
-/**
- * Servidor Express para a aplicação EduChatConnect
- *
- * Este arquivo inicia um servidor Express que serve os arquivos estáticos
- * e lida com as requisições API da aplicação.
- */
-import '../src/dom-patch.js';
